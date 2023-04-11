49e82d7d8d5726fd5c59017b1764e0ab6dc5ff0f
Hookiks <58189468+Hookiks@users.noreply.github.com>
2023-04-11 08:53:30 +0200

    Add files via upload

---
+++ Projet Bataille Naval/SQL Import par Python.py
@@ -0,0 +1,9 @@
+import sqlite3
+connexion = sqlite3.connect("basededonnees.db")
+connexion.close()
+curseur_sql = connexion.cursor()  # Récupération d'un curseur
+curseur_sql.execute('''CREATE TABLE IF NOT EXISTS score(
+    id INTEGRER PRIMARY KEY AUPINCREMENT UNIQUE,
+    Nom TEXT,
+    score INT
+)''')
