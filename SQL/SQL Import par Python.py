f83ad1de1631fb46019439bbddd17b43bd1e8c35
Hookiks <58189468+Hookiks@users.noreply.github.com>
2023-04-11 20:51:25 +0200

    Delete SQL Import par Python.py

--- SQL/SQL Import par Python.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-connexion = sqlite3.connect("basededonnees.db")
-connexion.close()
-curseur_sql = connexion.cursor()  # Récupération d'un curseur
-curseur_sql.execute('''CREATE TABLE IF NOT EXISTS score(
-    id INTEGRER PRIMARY KEY AUPINCREMENT UNIQUE,
-    Nom TEXT,
-    score INT
-)''')
